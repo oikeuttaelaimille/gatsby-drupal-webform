34b610c9029706a80af250509d082f4da08b2702
Santeri Hurnanen <santeri@oikeuttaelaimille.fi>
2019-08-21 02:40:52 +0300

    fix: fix type generation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export { default } from './Webform'
+import Form from './Form'
+import FormGroup from './FormGroup'
+import Input from './Input'
+import InputGroup from './InputGroup'
+import Textarea from './Textarea'
+import Checkbox from './Checkbox'
+import Button from './Button'
+import Webform, { WebformObject } from './Webform'
+import { formToJSON } from './utils'
+
+export { WebformObject, Form, FormGroup, Input, InputGroup, Textarea, Button, Checkbox, formToJSON }
+export default Webform
