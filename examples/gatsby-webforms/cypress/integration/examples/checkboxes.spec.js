e290c8de243bba9159bbd17aa39b3b4e698079bd
Santeri Hurnanen <santeri@oikeuttaelaimille.fi>
2021-02-18 17:01:52 +0200

    build: update drupal example to drupal 9

--- examples/gatsby-webforms/cypress/integration/examples/checkboxes.spec.js
+++ examples/gatsby-webforms/cypress/integration/examples/checkboxes.spec.js
@@ -7,16 +7,12 @@ context('Checkbox serialization', () => {
 	})
 
 	it('test serialization', () => {
-		cy.server()
-			.route('POST', Cypress.env('ENDPOINT'))
-			.as('formSubmit')
+		cy.server().route('POST', Cypress.env('ENDPOINT')).as('formSubmit')
 
-		cy.get('@webform').submit()
+		cy.get('button').click()
 
 		// Wait for response.status to be 200
-		cy.wait('@formSubmit')
-			.its('status')
-			.should('be', 200)
+		cy.wait('@formSubmit', { timeout: 10000 }).its('status').should('be', 200)
 
 		cy.get('@formSubmit')
 			.its('requestBody') // alternative: its('request.body')
